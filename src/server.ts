8dca55b8467daf9dfa56c8e690692be51bb926fc
Monwar Hossain <monwarhossainmunna@gmail.com>
2023-06-17 23:13:50 +0600

    setup express mongoose dotenv

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config/index";
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string),
+      console.log("🛢 Database connection established");
+    app.listen(config.port, () => {
+      console.log(`Application app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log("Failed connecting to database", error);
+  }
+}
+main();
